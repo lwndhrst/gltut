3789957bc7a17274dd693c770a31c50fec512106
Leon Windhorst <leon@windhorst.dev>
2024-05-21 09:11:29 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G "Unix Makefiles"
+cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles"
 cmake --build build
